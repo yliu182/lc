6194cc5a4770524eb1cdc84669ae16a09bb63f41
yliu182 <yliu182@gmail.com>
2026-08-23 15:13:35 +0800

    p1 second

--- hot100/001_two_sum_1/solution.py
+++ hot100/001_two_sum_1/solution.py
@@ -30,26 +30,48 @@ nums =         [3, 2, 4]
                    i
 
 sorted_nums =  [2, 3, 4]
-                i 
+                i
 
 """
 class Solution:
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     if len(nums) == 0:
+    #         return []
+
+    #     # sort the list, while keep the index
+    #     num_tups = [(nums[i], i) for i in range(len(nums))]
+    #     num_tups.sort(key = lambda x: x[0])
+    #     l = 0
+    #     r = len(num_tups) - 1
+    #     while l < r:
+    #         s = num_tups[l][0] + num_tups[r][0]
+    #         if s == target:
+    #             return [num_tups[l][1], num_tups[r][1]]
+    #         elif s < target:
+    #             l += 1
+    #         else:
+    #             r -= 1
+
+    #     return []
+
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        if len(nums) == 0:
-            return []
-            
-        # sort the list, while keep the index
-        num_tups = [(nums[i], i) for i in range(len(nums))]
-        num_tups.sort(key = lambda x: x[0])
+        if len(nums) <= 1:
+            return nums
+
+        sorted_nums = []
+        for i, v in enumerate(nums):
+            sorted_nums.append((v, i))
+        sorted_nums.sort(key=lambda x : x[0])
         l = 0
-        r = len(num_tups) - 1
+        r = len(sorted_nums) - 1
         while l < r:
-            s = num_tups[l][0] + num_tups[r][0]
+            s = sorted_nums[l][0] + sorted_nums[r][0]
             if s == target:
-                return [num_tups[l][1], num_tups[r][1]]
+                return [sorted_nums[l][1], sorted_nums[r][1]]
             elif s < target:
-                l += 1
+                l = l + 1
             else:
-                r -= 1
+                r = r - 1
 
         return []
