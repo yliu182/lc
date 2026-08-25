2b496f7a15d0be7c5a255a1c1c05b02800121739
yliu182 <yliu182@gmail.com>
2026-08-25 13:15:24 +0800

    t3_version1

--- hot100/003_longest_substring_without_repeating_characters_3/solution.py
+++ hot100/003_longest_substring_without_repeating_characters_3/solution.py
@@ -23,4 +23,36 @@ Example 3:
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        pass
+        if len(s) <= 1:
+            return len(s)
+
+        left = 0
+        right = 1
+        dedup_set = {s[0]}
+        max_result = 1
+        while right < len(s):
+            if s[right] not in dedup_set:
+                dedup_set.add(s[right])
+                right += 1
+                max_result = max(max_result, len(dedup_set))
+            else:
+                first_ocur_idx = s[left:right].find(s[right])
+                for i in range(first_ocur_idx + 1 - left):
+                    dedup_set.remove(s[i + left])
+                left = first_ocur_idx + 1
+                dedup_set.add(s[right])
+                right += 1
+
+        return max_result
+
+
+
+# "bbbb"
+#  l
+#   r
+
+# left  1
+# right 2
+# first_ocur_idx 0
+# max_result
+# dedup_set {b}
