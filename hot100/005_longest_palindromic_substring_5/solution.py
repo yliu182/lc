9716ce9c5c2a380eeda1f6d2d4db65ccf6a88166
yliu182 <yliu182@gmail.com>
2026-08-28 17:14:46 +0800

    lc5_version_1

--- hot100/005_longest_palindromic_substring_5/solution.py
+++ hot100/005_longest_palindromic_substring_5/solution.py
@@ -15,4 +15,29 @@ Example 2:
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        pass
+        def expand(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return left+1, right-1
+
+        if len(s) < 2:
+            return s
+
+        final_left = 0
+        final_right = 0
+
+        for center in range(len(s)):
+            left, right = expand(center, center)
+            if right - left > final_right - final_left:
+                final_right = right
+                final_left = left
+
+        for start in range(len(s)-1):
+            if s[start] == s[start+1]:
+                left, right = expand(start, start+1)
+                if right - left > final_right - final_left:
+                    final_right = right
+                    final_left = left
+
+        return s[final_left : final_right + 1]
