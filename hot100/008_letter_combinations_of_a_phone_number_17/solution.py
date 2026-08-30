11b72a8477f1150a1cd5b541e0486a744016e0f4
yliu182 <yliu182@gmail.com>
2026-08-30 10:08:39 +0800

    lc8_version_1

--- hot100/008_letter_combinations_of_a_phone_number_17/solution.py
+++ hot100/008_letter_combinations_of_a_phone_number_17/solution.py
@@ -25,4 +25,53 @@ from typing import List
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        pass
+
+        m = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        if len(digits) == 0:
+            return []
+
+        final_results = []
+
+        # backtrace version 1, using list as "path", we are mutating the same
+        # list in place, not create new list, so we have to pop.
+        def bt_v1(
+            index, # int
+            path, # List[str]
+        ):
+            if index == len(digits):
+                final_results.append("".join(path))
+                return
+
+            letter = digits[index]
+            for s in m[letter]:
+                path.append(s)
+                bt_v1(index + 1, path)
+                path.pop()
+
+        # backtrace version 2, using str as "path", we are creating new string
+        # for each iteration.
+        def bt_v2(
+            index, # int
+            path, # str
+        ):
+            if index == len(digits):
+                final_results.append(path)
+                return
+
+            letter = digits[index]
+            for s in m[letter]:
+                bt_v2(index + 1, path + s)
+
+        # bt_v1(0, [])
+        bt_v2(0, "")
+        return final_results
