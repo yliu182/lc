17199eb9d4829a7d41c44ba8b267d3bec9460cc5
yliu182 <yliu182@gmail.com>
2026-08-30 11:06:11 +0800

    lc9_version_1

--- hot100/009_remove_nth_node_from_end_of_list_19/solution.py
+++ hot100/009_remove_nth_node_from_end_of_list_19/solution.py
@@ -28,16 +28,35 @@ class ListNode:
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        p1 = head
-        p2 = head
-        for i in range(n):
-            if p2 is not null:
-                p2 = p2->next
+        if head is None:
+            return None
+
+        dummy = ListNode(0, head)
+
+        p1 = dummy
+        p2 = dummy
+        # p2 先走 n + 1 步
+        for _ in range(n+1):
+            if p2 is not None:
+                p2 = p2.next
             else:
                 return head
+        # 然后一起走，直到 p2 走到 None
+        while p2 is not None:
+            p1 = p1.next
+            p2 = p2.next
 
-        while p2 is not null:
-            p2 = p2->next
-            if p2 is null:
-                p1->next = p1->next->next
-                return head
+        # p1 此时正好在待删除节点的前一个位置
+        p1.next = p1.next.next
+
+        return dummy.next
+"""
+    [1, 2, 3, 4, 5]  n =2
+     p1    p2
+        p1    p2
+            1.   2
+
+
+有一个edge case 是说 p2 先走了n 步之后，p1 刚好需要被删掉，需要删 head.
+
+"""
