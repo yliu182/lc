f88aee10c0d6f7894325c6b016f9855be0a239ff
yliu182 <yliu182@gmail.com>
2026-08-30 10:20:08 +0800

    lc9_version_1

--- hot100/009_remove_nth_node_from_end_of_list_19/solution.py
+++ hot100/009_remove_nth_node_from_end_of_list_19/solution.py
@@ -28,4 +28,16 @@ class ListNode:
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        pass
+        p1 = head
+        p2 = head
+        for i in range(n):
+            if p2 is not null:
+                p2 = p2->next
+            else:
+                return head
+
+        while p2 is not null:
+            p2 = p2->next
+            if p2 is null:
+                p1->next = p1->next->next
+                return head
