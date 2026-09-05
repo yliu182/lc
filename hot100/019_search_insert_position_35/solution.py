708dadfba963e37d0a1e194501862afe5fa95fb7
yliu182 <yliu182@gmail.com>
2026-09-05 17:37:53 +0800

    lc19_version_2

--- hot100/019_search_insert_position_35/solution.py
+++ hot100/019_search_insert_position_35/solution.py
@@ -23,6 +23,26 @@ Example 3:
 from typing import List
 
 class Solution:
+    # def searchInsert(self, nums: List[int], target: int) -> int:
+    #     l = 0
+    #     r = len(nums) - 1
+    #     while l <= r:
+    #         m = (l + r) // 2
+    #         if nums[m] == target:
+    #             return m
+    #         elif target < nums[m] and m > 0 and target > nums[m-1]:
+    #             return m
+    #         elif target < nums[m-1]:
+    #             r = m - 1
+    #         elif target > nums[m] and m < len(nums) - 1 and target < nums[m+1]:
+    #             return m+1
+    #         else:
+    #             l = m + 1
+
+    #     if m == len(nums) - 1:
+    #         return len(nums)
+    #     return m
+
     def searchInsert(self, nums: List[int], target: int) -> int:
         l = 0
         r = len(nums) - 1
@@ -30,18 +50,12 @@ class Solution:
             m = (l + r) // 2
             if nums[m] == target:
                 return m
-            elif target < nums[m] and m > 0 and target > nums[m-1]:
-                return m
-            elif target < nums[m-1]:
-                r = m - 1
-            elif target > nums[m] and m < len(nums) - 1 and target < nums[m+1]:
-                return m+1
+            elif target < nums[m]:
+                r = m -1
             else:
                 l = m + 1
 
-        if m == len(nums) - 1:
-            return len(nums)
-        return m
+        return l
 
 """
 input  = [1,3,5,6],
@@ -53,7 +67,6 @@ m = 1
 
 nums[m] = 3 < target
 the results should be in rignt side, throw away left side
-
 
 l = m + 1 = 2
 m = 2
