d70f813307ac646f032188df9a7c224dfdc6b516
yliu182 <yliu182@gmail.com>
2026-09-07 12:22:24 +0800

    lc21_version_1

--- hot100/021_first_missing_positive_41/solution.py
+++ hot100/021_first_missing_positive_41/solution.py
@@ -26,5 +26,41 @@ from typing import List
 
 
 class Solution:
+    """
+    uses O(1) auxiliary space.
+       - likely to use in-place SWAP to put data into list
+
+    assume n = len(nums)
+    the answer must be an integer in the range of [1, n+1]
+
+    sort the nums in an efficient way
+
+    sort:    O(n log n) complexity
+
+    Sorting gives me a simple O(n log n) solution, but since the problem requires O(n), I'll need to use the array itself to encode whether each value from 1 to n exists
+
+
+
+
+    while the current element is not placed into its desired position:
+        swap
+
+    最容易漏掉的是这个条件：nums[nums[i] - 1] != nums[i], 它是为了处理重复数字。
+    如果没有这个条件, 你会一直：swap 1 和 1
+
+    """
     def firstMissingPositive(self, nums: List[int]) -> int:
-        pass
+        n = len(nums)
+        for i, val in enumerate(nums):
+            cur = val
+            while cur > 0 and cur <= n:
+                target_pos = cur - 1
+                cur, nums[target_pos] = nums[target_pos], cur
+                if nums[target_pos] == cur:
+                    break
+
+        for i, n in enumerate(nums):
+            if n != i+1:
+                return i+1
+
+        return n+1
