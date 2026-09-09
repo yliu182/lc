c04304b76387a091f0130c5b2178cd098bc0ca35
yliu182 <yliu182@gmail.com>
2026-09-09 13:28:24 +0800

    lc29_version_1

--- hot100/029_spiral_matrix_54/solution.py
+++ hot100/029_spiral_matrix_54/solution.py
@@ -21,6 +21,48 @@ Constraints:
 from typing import List
 
 
+"""
+维护 四个边界
+→ top row
+↓ right col
+← bottom row
+↑ left col
+走完一条边，就把对应边界往里面缩一格。
+
+
+"""
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        pass
+        if len(matrix) < 1 or len(matrix[0]) < 1:
+            return []
+
+        top = 0
+        bottom = len(matrix) - 1
+        left = 0
+        right = len(matrix[0]) - 1
+
+        output = []
+        while top <= bottom and left <= right:
+            # → top row
+            for i in range(left, right+1, 1):
+                output.append(matrix[top][i])
+            top += 1
+
+            # ↓ right col
+            for i in range(top, bottom+1, 1):
+                output.append(matrix[i][right])
+            right -= 1
+
+            if bottom >= top:
+                # ← bottom row
+                for i in range(right, left-1, -1):
+                    output.append(matrix[bottom][i])
+                bottom -= 1
+
+            if left <= right:
+                # ↑ left col
+                for i in range(bottom, top-1, -1):
+                    output.append(matrix[i][left])
+                left += 1
+
+        return output
