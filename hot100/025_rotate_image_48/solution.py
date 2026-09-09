099c17aa294147bb4fd5c507497a21c43b0fdd60
yliu182 <yliu182@gmail.com>
2026-09-09 08:44:03 +0800

    lc25_version_1

--- hot100/025_rotate_image_48/solution.py
+++ hot100/025_rotate_image_48/solution.py
@@ -27,7 +27,35 @@ Constraints:
 
 from typing import List
 
+"""
+1 2 3
+4 5 6
+7 8 9
+
+7 4 1
+8 5 2
+9 6 3
+
+[2][0] -> [0][0]
+[2][2] -> [2][0]
+[2][1] -> [1][0]
+
+把旋转拆成两步：
+
+沿主对角线翻转（transpose）
+
+每一行左右翻转（reverse each row）
+
+"""
 
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        pass
+        # transpose
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i+1, n, 1):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for i in range(n):
+            matrix[i].reverse()
+        return matrix
