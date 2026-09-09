ec2392c7def3b270b371340715ab96bd4c808a57
yliu182 <yliu182@gmail.com>
2026-09-09 09:02:16 +0800

    lc26_version_1

--- hot100/026_group_anagrams_49/solution.py
+++ hot100/026_group_anagrams_49/solution.py
@@ -27,7 +27,34 @@ Constraints:
 
 from typing import List
 
+"""
+dict("sorted letters concat into a str" -> [])
+
+这个方法的复杂度是：假设一共有 n 个字符串，每个字符串平均长度为 k，
+Time:  O(n * k log k)
+Space: O(n * k)
+"""
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        pass
+        mapping = {}
+        for s in strs:
+            key = "".join(sorted(s))
+            """
+            Python 的 string 本身就可以直接遍历：
+            sorted("eat") -> ['a', 'e', 't']
+
+            sort() 和 sorted() 不一样
+            x = ['e', 'a', 't']
+            x.sort() will change x inplace
+            sorted(x) will create a new list
+            """
+            if key not in mapping:
+                mapping[key] = [s]
+            else:
+                mapping[key].append(s)
+
+        final_return = []
+        for v in mapping.values():
+            final_return.append(v)
+        return final_return
