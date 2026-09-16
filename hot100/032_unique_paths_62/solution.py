890610738e98b77480105133f7726ed7cd5710a0
yliu182 <yliu182@gmail.com>
2026-09-16 13:07:59 +0800

    d32 version 2

--- hot100/032_unique_paths_62/solution.py
+++ hot100/032_unique_paths_62/solution.py
@@ -23,21 +23,59 @@ Constraints:
 
 
 class Solution:
+    # def uniquePaths(self, m: int, n: int) -> int:
+    #     def dfs(
+    #         r_remain,
+    #         d_remain,
+    #     ):
+    #         if r_remain == 0 and d_remain == 0:
+    #             return 1
+
+    #         result = 0
+    #         if r_remain > 0:
+    #             result += dfs(r_remain - 1, d_remain)
+
+    #         if d_remain > 0:
+    #             result += dfs(r_remain, d_remain - 1)
+
+    #         return result
+
+    #     return dfs(m - 1, n - 1)
+
+
+    # def uniquePaths(self, m: int, n: int) -> int:
+    #     # 假设计算：
+    #     # dfs(2, 2)
+    #     # 递归关系是：
+    #     # dfs(2, 2) = dfs(1, 2) + dfs(2, 1)
+    #     # 继续展开：
+    #     # dfs(1, 2)
+    #     # ├── dfs(0, 2)
+    #     # └── dfs(1, 1)
+
+    #     # dfs(2, 1)
+    #     # ├── dfs(1, 1)
+    #     # └── dfs(2, 0)
+    #     # 可以看到：
+    #     # dfs(1, 1)
+    #     # 被计算了两次。网格越大，这种重复越严重。
+    #     @cache
+    #     def dfs(
+    #         r_remain,
+    #         d_remain,
+    #     ):
+    #         if r_remain == 0 or d_remain == 0:
+    #             return 1
+
+    #         return dfs(r_remain - 1, d_remain) + dfs(r_remain, d_remain - 1)
+
+    #     return dfs(m - 1, n - 1)
+
     def uniquePaths(self, m: int, n: int) -> int:
-        def dfs(
-            r_remain,
-            d_remain,
-        ):
-            if r_remain == 0 and d_remain == 0:
-                return 1
+        dp = [[1] * n for _ in range(m)]
 
-            result = 0
-            if r_remain > 0:
-                result += dfs(r_remain - 1, d_remain)
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
 
-            if d_remain > 0:
-                result += dfs(r_remain, d_remain - 1)
-
-            return result
-
-        return dfs(m - 1, n - 1)
+        return dp[m-1][n-1]
