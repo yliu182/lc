da6fadcd8dcb9574e824926791d6b8f737a9b80e
yliu182 <yliu182@gmail.com>
2026-09-17 10:30:58 +0800

    038 solution 2

--- hot100/038_sort_colors_75/solution.py
+++ hot100/038_sort_colors_75/solution.py
@@ -28,24 +28,56 @@ from typing import List
 from collections import defaultdict
 
 class Solution:
+
     # easy solution
     # 时间：O(n)
     # 额外空间：O(1)
+    # def sortColors(self, nums: List[int]) -> None:
+    #     cnt_map = defaultdict(int)
+    #     for n in nums:
+    #         cnt_map[n] += 1
+
+    #     cur_pos = 0
+    #     for _ in range(cnt_map[0]):
+    #         nums[cur_pos] = 0
+    #         cur_pos += 1
+
+    #     for _ in range(cnt_map[1]):
+    #         nums[cur_pos] = 1
+    #         cur_pos += 1
+
+    #     for _ in range(cnt_map[2]):
+    #         nums[cur_pos] = 2
+    #         cur_pos += 1
 
     def sortColors(self, nums: List[int]) -> None:
-        cnt_map = defaultdict(int)
-        for n in nums:
-            cnt_map[n] += 1
+        """
+        left      i       right
+         ↓        ↓       ↓
+[ 0 0 0 | 1 1 1 | ? ? ? ? ? | 2 2 2 ]
+  已搞定   已搞定    待检查       已搞定
 
-        cur_pos = 0
-        for _ in range(cnt_map[0]):
-            nums[cur_pos] = 0
-            cur_pos += 1
 
-        for _ in range(cnt_map[1]):
-            nums[cur_pos] = 1
-            cur_pos += 1
+  [0 区域] [1 区域] [未知区域] [2 区域]
+           ↑        ↑
+          left      i
+                            ↑
+                            right (the expected position of next 2)
+        """
 
-        for _ in range(cnt_map[2]):
-            nums[cur_pos] = 2
-            cur_pos += 1
+        left = 0
+        i = 0
+        right = len(nums) - 1
+
+        while i <= right:
+            if nums[i] == 0:
+                # swap nums[left] with nums[i]
+                nums[left], nums[i] = nums[i], nums[left]
+                left += 1
+                i += 1
+            elif nums[i] == 1:
+                i += 1
+            else:
+                # swap nums[i] with nums[right] (right is the expected position of the next 2)
+                nums[i], nums[right] = nums[right], nums[i]
+                right -= 1
