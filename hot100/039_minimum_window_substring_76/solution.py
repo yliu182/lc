ea65ec4d0806c044d9f2b19b5a84c55d128190ef
yliu182 <yliu182@gmail.com>
2026-09-17 12:40:38 +0800

    039 solution 1

--- hot100/039_minimum_window_substring_76/solution.py
+++ hot100/039_minimum_window_substring_76/solution.py
@@ -26,7 +26,75 @@ Constraints:
     s and t consist of uppercase and lowercase English letters.
 """
 
+from collections import defaultdict
 
 class Solution:
+    """
+    right：负责扩大窗口
+    left：负责缩小窗口
+    整体节奏是：
+    right 加入一个字符
+        ↓
+    检查窗口是否有效
+        ↓
+    如果有效，left 不断向右缩小
+        ↓
+    窗口变得无效
+        ↓
+    right 再加入下一个字符
+
+    对应结构：
+
+    for right in range(len(s)):
+        # 把 s[right] 加入窗口
+
+        while 窗口有效:
+            # 更新最短答案
+            # 移除 s[left]
+            left += 1
+    """
     def minWindow(self, s: str, t: str) -> str:
-        pass
+        target_map = defaultdict(int)
+        for c in t:
+            target_map[c] += 1
+
+        matched_cnt = 0 # number of distinct character c that have matched_map[c] == target_map[c]
+        matched_map = defaultdict(int)
+        left = 0
+
+        min_result_length = float('inf')
+        min_result_str = ""
+
+        for right in range(len(s)):
+            if s[right] in target_map:
+                matched_map[s[right]] += 1
+                if matched_map[s[right]] == target_map[s[right]]:
+                    matched_cnt += 1
+
+            while matched_cnt == len(target_map):
+                if min_result_length > right - left + 1:
+                    min_result_str = s[left : right + 1]
+                    min_result_length = right - left + 1
+
+                # move left ptr, shrink the window until it does not meet the requirement
+                c = s[left]
+                if c in target_map:
+                    matched_map[c] -= 1
+                    if matched_map[c] < target_map[c]:
+                        # substring [left, right] is a solution candidate, further increasing left will disquality
+                        matched_cnt -= 1
+                left += 1
+
+        return min_result_str
+
+
+"""
+    Input: s = "abc", t = "abc"
+    Output: ""
+
+target_map = {'a': 1, 'b': 1, 'c': 1}
+
+matched_map = {'a': 1}, matched_cnt = 1
+
+
+"""
