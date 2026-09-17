955311fcd1040274f353bc3f2e20511a7a8d329c
yliu182 <yliu182@gmail.com>
2026-09-17 13:21:00 +0800

    040 solution 1,2

--- hot100/040_subsets_78/solution.py
+++ hot100/040_subsets_78/solution.py
@@ -25,5 +25,62 @@ from typing import List
 
 
 class Solution:
+    # def subsets(self, nums: List[int]) -> List[List[int]]:
+    #     solution = []
+
+    #     """
+    #     当前时间复杂度
+    #     一共有 2^n个子集。
+    #     每个叶子处，你需要遍历长度为 n 的 path： for i, p in enumerate(path):
+    #     因此时间复杂度是： O(n × 2^n)
+    #     """
+    #     def dfs(path):
+    #         if len(path) == len(nums):
+    #             candidate = []
+    #             for i, p in enumerate(path):
+    #                 if p == 1:
+    #                     candidate.append(nums[i])
+    #             solution.append(candidate)
+    #             return
+
+    #         dfs(path + [0]) # exclude the next elements in nums
+    #         dfs(path + [1]) # include
+
+    #     dfs([])
+    #     return solution
+
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        pass
+        solution = []
+        """
+        就是经典回溯：
+        做出选择
+        递归
+        撤销选择
+
+        这种方式的优点是：
+        - 不需要维护 0/1 标记
+        - 不需要在叶子重新遍历标记构造子集
+        - 不需要每次递归都执行 path + [...]
+        - 递归含义更直接
+
+        不过叶子处的：path.copy()
+        """
+
+        path = []
+        solution = []
+        def dfs(i):
+            # i is the next index to process
+            if i == len(nums):
+                solution.append(path.copy())
+                return
+
+            # option 1, does not include nums[i] into the path
+            dfs(i+1)
+
+            # option 2, include nums[i] into the path
+            path.append(nums[i])
+            dfs(i+1)
+            path.pop()
+
+        dfs(0)
+        return solution
