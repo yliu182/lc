f7f556527b298053b3d8caa71ff0e7f0b70950ca
yliu182 <yliu182@gmail.com>
2026-09-19 10:28:47 +0800

    044 solution 1

--- hot100/044_unique_binary_search_trees_96/solution.py
+++ hot100/044_unique_binary_search_trees_96/solution.py
@@ -15,8 +15,24 @@ Example 2:
 Constraints:
     1 <= n <= 19
 """
-
+from collections import defaultdict
 
 class Solution:
     def numTrees(self, n: int) -> int:
-        pass
+        dp = defaultdict(int) # dp[i]: the number of options with i nodes in total
+        dp[0] = 1
+        dp[1] = 1
+
+        # 1,...i-1      i       i+1, ... n
+
+        # dp[2]
+        # dp[2] = dp[1] + dp[0] = 2
+        # dp[3] = dp[0] * dp[2] + dp[1] * dp[1]  + dp[2] * dp[0] = 1 * 2 + 1 * 1 + 1 * 2 = 5
+        #          1 be root       2 be root.       3 being root
+
+        for i in range(2, n+1):
+            for r in range(1, i+1):
+                # [1, 2, ... r-1] in left subtree,  r is root,  [r+1, ...., i] being right sub-tree
+                dp[i] += dp[r-1] * dp[i - r]
+
+        return dp[n]
