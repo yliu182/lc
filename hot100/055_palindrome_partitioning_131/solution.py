10798c8ad9182de45fa765817582542f3c21a735
yliu182 <yliu182@gmail.com>
2026-09-21 15:21:32 +0800

    055 solution 1

--- hot100/055_palindrome_partitioning_131/solution.py
+++ hot100/055_palindrome_partitioning_131/solution.py
@@ -21,5 +21,59 @@ from typing import List
 
 
 class Solution:
+    """
+    这是 yao 自己写的一个版本，有bug
+
     def partition(self, s: str) -> List[List[str]]:
-        pass
+        global_results = []
+
+        def dfs(idx, path):
+            # return a list of list of strings
+            nonlocal global_results
+            if idx == len(s):
+                global_results.append(path)
+
+                #这里有一个严重的 bug, 在找到答案的时候，需要copy 固定下来。不然以后会被后续的 pop 影响
+                return
+            cur = idx
+            while cur < len(s) and s[cur] == s[idx]:
+                path.append(s[idx:cur+1])
+                dfs(cur + 1, path)
+                path.pop()
+                cur += 1
+
+        dfs(0, [])
+        return global_results
+    """
+
+    def partition(self, s: str) -> List[List[str]]:
+        global_results = []
+
+        def isPalindrome(s):
+            if len(s) == 1:
+                return True
+            left = 0
+            right = len(s) - 1 - left
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left += 1
+                right -= 1
+            return True
+
+        def dfs(idx, path):
+            if idx == len(s):
+                global_results.append(path.copy())
+                return
+
+            cur = idx
+            # iterate cur from [idx, len(s)-11], check if s[idx, cur] is Palindrome
+            for cur in range(idx, len(s)):
+                substr = s[idx:cur+1]
+                if isPalindrome(substr):
+                    path.append(substr)
+                    dfs(cur + 1, path)
+                    path.pop()
+
+        dfs(0, [])
+        return global_results
